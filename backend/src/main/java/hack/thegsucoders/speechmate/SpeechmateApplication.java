72a4666a3075bf9b5153b7c68f7641e3a36871a5
Raphael Omorose <raphaelomorose@gmail.com>
2025-11-07 19:10:44 -0500

    Initialize backend project structure with Maven and Spring Boot setup

---
+++ backend/src/main/java/hack/thegsucoders/speechmate/SpeechmateApplication.java
@@ -0,0 +1,13 @@
+package hack.thegsucoders.speechmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechmateApplication.class, args);
+	}
+
+}
