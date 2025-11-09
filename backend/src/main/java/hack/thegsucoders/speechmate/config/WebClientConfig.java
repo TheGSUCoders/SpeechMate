403fe0b0eff747c85fa72320fc31d5ac81c094b4
Raphael Omorose <raphaelomorose@gmail.com>
2025-11-09 01:05:52 -0500

    feat: Configure WebClient with custom exchange strategies for improved memory handling

--- backend/src/main/java/hack/thegsucoders/speechmate/config/WebClientConfig.java
+++ backend/src/main/java/hack/thegsucoders/speechmate/config/WebClientConfig.java
@@ -2,12 +2,19 @@ package hack.thegsucoders.speechmate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
+
+	@Bean
+	public WebClient.Builder webClientBuilder() {
+		ExchangeStrategies strategies = ExchangeStrategies.builder()
+			.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+			.build();
+
+		return WebClient.builder()
+			.exchangeStrategies(strategies);
+	}
 }
